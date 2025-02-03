cd72be591a943b82808f72be181c7c995ed2a5e3
Yeonjae Choi <fox2yj@naver.com>
2025-02-03 11:30:19 +0900

    [feat] 이미지 저장 기능 구현 (및 공유 기능 오류로 인한 삭제)

--- components/share/ShareButtons.tsx
+++ components/share/ShareButtons.tsx
@@ -1,28 +1,56 @@
-import React from 'react';
-import {Alert, Button, Image} from 'react-native';
+import React, { useState, useRef } from 'react';
+import {Button, Image, View, StyleSheet} from 'react-native';
 import Share from 'react-native-share';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import * as MediaLibrary from 'expo-media-library'; // 이미지 저장에 필요
+import { captureRef } from 'react-native-view-shot'; // 이미지 저장에 필요
 
 const ShareExample = () => {
-  const onShare = async () => {
+  const PlaceholderImage = require('@/assets/images/background-image.png');
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  const imageRef = useRef<View>(null);
+
+  if (status === null) {
+    requestPermission();
+  }
+
+  const onSaveImageAsync = async () => {
     try {
-      const result = await Share.open({
-        message:
-          'naver!!',
-        url:
-            'http://m.naver.com',
+      const localUri = await captureRef(imageRef, {
+        format: "jpg",
+        quality: 1.0,
       });
-    } catch (error: any) {
-      Alert.alert(error.message);
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert('Saved!');
+      }
+    } catch (e) {
+      console.log(e);
     }
   };
+  
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Button onPress={onShare} title="Share" />
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView style={styles.imageContainer}>  
+        <View ref={imageRef} collapsable={false}>
+          <Image source={PlaceholderImage} />
+        </View>
+        <Button onPress={onSaveImageAsync} title="Save" />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
 
 export default ShareExample;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  }
+});
